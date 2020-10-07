886c851030108f5eb592a873c58188d2abbaac09
Fabiano Ramos <fabiano.suporteinfo@gmail.com>
2020-10-07 12:24:09 -0300

    Add controllers

--- src/modules/users/infra/http/routes/users.routes.ts
+++ src/modules/users/infra/http/routes/users.routes.ts
@@ -1,61 +1,23 @@
 import { Router } from 'express';
 import multer from 'multer';
 import uploadConfig from '@config/upload';
-import { container } from 'tsyringe';
-
-import CreateUserService from '@modules/users/services/CreateUserService';
-import UpdateUserAvatarService from '@modules/users/services/UpdateUserAvatarService';
 
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
+import UsersController from '../controllers/UsersController';
+import UserAvatarController from '../controllers/UserAvatarController';
 
 const usersRouter = Router();
+const usersController = new UsersController();
+const userAvatarController = new UserAvatarController();
 const upload = multer(uploadConfig);
 
-usersRouter.post('/', async (request, response) => {
-  try {
-    const { name, email, password } = request.body;
-
-    // const usersRepository = new UsersRepository();
-    const createUser = container.resolve(CreateUserService);
-
-    const user = await createUser.execute({
-      name,
-      email,
-      password,
-    });
-
-    /**
-     * Nao mostrar a senha no retorno da criação do usuário
-     * delete user.password;
-     */
-    delete user.password;
-
-    return response.json(user);
-  } catch (err) {
-    return response.status(400).json({ error: err.message });
-  }
-});
+usersRouter.post('/', usersController.create);
 
 usersRouter.patch(
   '/avatar',
   ensureAuthenticated,
   upload.single('avatar'),
-  async (request, response) => {
-    try {
-      const userUpdateAvatar = container.resolve(UpdateUserAvatarService);
-
-      const user = await userUpdateAvatar.execute({
-        user_id: request.user.id,
-        avatar_filename: request.file.filename,
-      });
-
-      delete user.password;
-
-      return response.json({ user });
-    } catch (err) {
-      return response.status(400).json({ error: err.message });
-    }
-  },
+  userAvatarController.update,
 );
 
 export default usersRouter;
