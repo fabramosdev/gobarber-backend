cd402190fab504d01db96f2935db41f57e715dd1
Fabiano Ramos <fabiano.suporteinfo@gmail.com>
2020-10-07 22:36:49 -0300

    Added test coverage

--- src/modules/appointments/repositories/fakes/FakeAppointmentsRepository.ts
+++ src/modules/appointments/repositories/fakes/FakeAppointmentsRepository.ts
@@ -1,4 +1,5 @@
 import { uuid } from 'uuidv4';
+import { isEqual } from 'date-fns';
 
 import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
 import ICreateAppointmentDTO from '@modules/appointments/dtos/ICreateAppointmentDTO';
@@ -9,8 +10,8 @@ class AppointmentRepository implements IAppointmentsRepository {
   private appointments: Appointment[] = [];
 
   public async findByDate(date: Date): Promise<Appointment | undefined> {
-    const findAppointment = this.appointments.find(
-      appointment => appointment.date === date,
+    const findAppointment = this.appointments.find(appointment =>
+      isEqual(appointment.date, date),
     );
     return findAppointment;
   }
@@ -20,10 +21,6 @@ class AppointmentRepository implements IAppointmentsRepository {
     date,
   }: ICreateAppointmentDTO): Promise<Appointment> {
     const appointment = new Appointment();
-
-    // appointment.id = uuid();
-    // appointment.date = date;
-    // appointment.provider.id = provider_id;
 
     Object.assign(appointment, { id: uuid(), date, provider_id });
 
